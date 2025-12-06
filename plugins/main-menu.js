561d3c4801d64b84fbb97af8f673ff6ebdfdf445
ᥣᥱ᥆xі𝗍᥆. <erenxz01@gmail.com>
2025-12-06 16:52:40 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     let neww = performance.now()
     let speed = (neww - old).toFixed(4)
 
-    let menuText = `> 👋🏻 .ৎ˚₊‧  *Hola*, ${m.sender.split('@')[0]} *Pasa Feliz Navidad ❄️*.
+    let menuText = `> 👋🏻 .ৎ˚₊‧  *Hola*, +${m.sender.split('@')[0]} *Pasa Feliz Navidad ❄️*.
 
  ִ \`I N F O - B O T\` ! ୧ ֹ 
    
